c76a7a3de0f708576f640f4b1ea4fd06eb989dea
rowi1de <robert@scalable.capital>
2019-11-16 02:56:18 +0100

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,37 @@
 import * as core from '@actions/core';
-import {wait} from './wait'
+import { context, GitHub } from '@actions/github'
 
-async function run() {
+
+export async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const
+      repoToken = core.getInput('repo-token', { required: true }),
+      issue: { owner: string; repo: string; number: number } = context.issue
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms, 10));
-    core.debug((new Date()).toTimeString())
+    if (context.payload.action !== 'opened') {
+      console.log('No pull request was opened, skipping')
+      return
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    const client = new GitHub(repoToken)
+
+    const reviewers = core.getInput('reviewers').split(',').map(a => a.trim())
+    const teamReviewers = core.getInput('team-reviewers').split(',').map(a => a.trim())
+
+    await client.pulls.createReviewRequest(
+      {
+        owner: issue.owner,
+        repo: issue.repo,
+        pull_number: issue.number,
+        reviewers: reviewers,
+        team_reviewers: teamReviewers
+      }
+    )
+
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
+    throw error
   }
 }
 
-run();
+run()
