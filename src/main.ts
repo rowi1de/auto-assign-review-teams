a8e8950266d7d418b41f8c4d7557a9951ab928cd
Zara Kay <zara.kay@bugcrowd.com>
2020-12-14 17:38:44 +1100

    dirty bit of code to add timtamteam as a reviewee

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
+const TEAM_ID = 3920492
+const TEAM_SLUG = 'timtamteam'
 
 export async function run() {
   try {
+    // get current repo context
     const
       repoToken = core.getInput('repo-token', { required: true }),
-      issue: { owner: string; repo: string; number: number } = github.context.issue
-      core.setSecret(repoToken);
+      issue = github.context.issue
+    core.setSecret(repoToken);
 
     if (issue == null || issue.number == null) {
       console.log('No pull request context, skipping')
@@ -17,7 +20,8 @@ export async function run() {
     //See https://octokit.github.io/rest.js/
     const client = new github.GitHub(repoToken)
 
-    const includeDraft : Boolean =  Boolean(core.getInput('include-draft') || false) 
+    // get current pull request
+    const includeDraft: Boolean = Boolean(core.getInput('include-draft') || false)
     const pull = await client.pulls.get(
       {
         owner: issue.owner,
@@ -27,55 +31,39 @@ export async function run() {
     )
 
     //Skip DRAFT PRs
-    if(pull.data.draft && !includeDraft){
+    if (pull.data.draft && !includeDraft) {
       console.log('Skipped: DRAFT Pull Request, not assigning PR.')
       return
     }
 
-    const skipWithNumberOfReviewers : number =  Number(core.getInput('skip-with-manual-reviewers') || Number.MAX_VALUE) 
+    // check if exists current reviewers
+    const skipWithNumberOfReviewers: number = Number(core.getInput('skip-with-manual-reviewers') || Number.MAX_VALUE)
     const numberOfReviewers = pull.data.requested_reviewers?.length || 0
-    if(numberOfReviewers >= skipWithNumberOfReviewers){
+    if (numberOfReviewers >= skipWithNumberOfReviewers) {
       console.log('Skipped: Already ' + numberOfReviewers + ' assigned reviewers, not assigning PR.')
       return
     }
 
-    const teams = core.getInput('teams').split(',').map(a => a.trim())
-    const persons = core.getInput('persons')
-      .split(',')
-      // filter out PR creator
-      .filter(user => user !== issue.owner)
-      .map(a => a.trim())
-    
-    if(teams.length == 0 && persons.length == 0){
-      core.setFailed("Please specify 'teams' and/or 'persons'")
+    // check if the owner is in timtamteam
+    try {
+      const membership = client.teams.getMembership({ team_id: TEAM_ID, username: issue.owner })
+    } catch {
+      console.log('Owner is not part of TimTamTeam')
       return
     }
 
-    if(persons.length > 0) {
-      console.log("Adding persons: " + persons)
-      const personResponse = await client.pulls.createReviewRequest(
-          {
-            owner: issue.owner,
-            repo: issue.repo,
-            pull_number: issue.number,
-            reviewers: persons
-          }
-      )
-      console.log("Request Status:" + personResponse.status + ", Persons: " + personResponse?.data?.requested_reviewers?.map(r => r.login).join(','))
-    }
+    // confirmed owner is part of timtamteam, assign team to it
+    console.log("Adding TimTamTeam")
+    const teamResponse = await client.pulls.createReviewRequest(
+      {
+        owner: issue.owner,
+        repo: issue.repo,
+        pull_number: issue.number,
+        team_reviewers: [TEAM_SLUG]
+      }
+    )
 
-    if(teams.length > 0) {
-      console.log("Adding teams: " + teams)
-      const teamResponse = await client.pulls.createReviewRequest(
-          {
-            owner: issue.owner,
-            repo: issue.repo,
-            pull_number: issue.number,
-            team_reviewers: teams
-          }
-      )
-      console.log("Request Status:" + teamResponse.status + ", Teams: " + teamResponse?.data?.requested_teams?.map(t => t.slug).join(','))
-    }
+    console.log("Request Status:" + teamResponse.status + ", Teams: " + teamResponse?.data?.requested_teams?.map(t => t.slug).join(','))
   } catch (error) {
     core.setFailed(error.message)
     throw error
