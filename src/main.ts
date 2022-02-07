03536acebe09e4516f2d6f1c97266b8678c2f992
Robert Wiesner <robert.wiesner@reev.com>
2022-02-07 22:22:21 +0100

    fix bool

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,30 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
-import {getBooleanInput} from "@actions/core";
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
+// eslint-disable-next-line require-jsdoc
 export async function run() {
   try {
-    const repoToken = core.getInput("repo-token", { required: true }),
-      issue: { owner: string; repo: string; number: number } =
-        github.context.issue;
+    const repoToken = core.getInput('repo-token', {required: true});
+    const issue: { owner: string; repo: string; number: number } =
+      github.context.issue;
     core.setSecret(repoToken);
 
-    const pickOneFromPersonsOrTeam =
-      core.getBooleanInput("pick-one-from-persons-or-team");
+    const pickOneFromPersonsOrTeam = core.getBooleanInput(
+        'pick-one-from-persons-or-team',
+        {required: false},
+    );
 
     if (issue == null || issue.number == null) {
-      console.log("No pull request context, skipping");
+      console.log('No pull request context, skipping');
       return;
     }
 
-    //See https://octokit.github.io/rest.js/
+    // See https://octokit.github.io/rest.js/
     const client = github.getOctokit(repoToken);
 
-    const includeDraft = core.getBooleanInput("include-draft");
+    const includeDraft = core.getBooleanInput('include-draft', {
+      required: false,
+    });
 
     const pull = await client.rest.pulls.get({
       owner: issue.owner,
@@ -28,47 +32,47 @@ export async function run() {
       pull_number: issue.number,
     });
 
-    //Skip DRAFT PRs
+    // Skip DRAFT PRs
     if (pull.data.draft && !includeDraft) {
-      console.log("Skipped: DRAFT Pull Request, not assigning PR.");
+      console.log('Skipped: DRAFT Pull Request, not assigning PR.');
       return;
     }
 
     const skipWithNumberOfReviewers: number = Number(
-      core.getInput("skip-with-manual-reviewers") || Number.MAX_VALUE
+        core.getInput('skip-with-manual-reviewers') || Number.MAX_VALUE,
     );
     const numberOfReviewers = pull.data.requested_reviewers?.length || 0;
     if (numberOfReviewers >= skipWithNumberOfReviewers) {
       console.log(
-        "Skipped: Already " +
+          'Skipped: Already ' +
           numberOfReviewers +
-          " assigned reviewers, not assigning PR."
+          ' assigned reviewers, not assigning PR.',
       );
       return;
     }
 
     const teams = core
-      .getInput("teams")
-      .split(",")
-      .map((a) => a.trim());
+        .getInput('teams')
+        .split(',')
+        .map((a) => a.trim());
     const persons = core
-      .getInput("persons")
-      .split(",")
-      // filter out PR creator
-      .filter((user) => user !== issue.owner)
-      .map((a) => a.trim());
+        .getInput('persons')
+        .split(',')
+    // filter out PR creator
+        .filter((user) => user !== issue.owner)
+        .map((a) => a.trim());
 
     if (teams.length == 0 && persons.length == 0) {
-      core.setFailed("Please specify 'teams' and/or 'persons'");
+      core.setFailed('Please specify \'teams\' and/or \'persons\'');
       return;
     }
 
     if (persons.length > 0) {
-      console.log("Picking from: " + persons);
-      const reviewers = pickOneFromPersonsOrTeam
-        ? [persons[Math.floor(Math.random() * persons.length)]]
-        : persons;
-      console.log("Adding person(s): " + reviewers);
+      console.log('Picking from: ' + persons);
+      const reviewers = pickOneFromPersonsOrTeam ?
+        [persons[Math.floor(Math.random() * persons.length)]] :
+        persons;
+      console.log('Adding person(s): ' + reviewers);
 
       const personResponse = await client.rest.pulls.requestReviewers({
         owner: issue.owner,
@@ -77,20 +81,23 @@ export async function run() {
         reviewers: reviewers,
       });
       console.log(
-        "Request Status:" +
+          'Request Status:' +
           personResponse.status +
-          ", Persons: " +
+          ', Persons: ' +
           personResponse?.data?.requested_reviewers
-            ?.map((r) => r.login)
-            .join(",")
+              ?.map((r) => r.login)
+              .join(','),
       );
     }
 
-    // Making sure that org is provided if user turns on pick-one-from-persons-or-team option and to use teams
-    const org: string = core.getInput("org");
+    // Making sure that org is provided
+    // if user turns on pick-one-from-persons-or-team
+    // option and to use teams
+    const org: string = core.getInput('org');
     if (pickOneFromPersonsOrTeam && teams.length > 0 && org == null) {
       core.setFailed(
-        "Please specify 'org' if you want to pick one from persons or teams and use Teams"
+          'Please specify \'org\' if you want to ' +
+          'pick one from persons or teams and use Teams',
       );
       return;
     }
@@ -98,19 +105,19 @@ export async function run() {
     if (teams.length > 0) {
       if (pickOneFromPersonsOrTeam) {
         // Picking out 1 person from first team listed
-        console.log("Selecting from first team provided: " + teams[0]);
+        console.log('Selecting from first team provided: ' + teams[0]);
         const members = await client.rest.teams.listMembersInOrg({
           org: org,
           team_slug: teams[0],
         });
         console.log(
-          "Request Status for getting team members:" + members.status
+            'Request Status for getting team members:' + members.status,
         );
         // filter out PR author
         const eligibleMembers = members.data
-          .filter((user) => user.login !== pull.data.user?.login)
-          .map((a) => a.login);
-        console.log("Picking reviewer from eligible members:", eligibleMembers);
+            .filter((user) => user.login !== pull.data.user?.login)
+            .map((a) => a.login);
+        console.log('Picking reviewer from eligible members:', eligibleMembers);
 
         const person = [
           eligibleMembers[Math.floor(Math.random() * eligibleMembers.length)],
@@ -123,15 +130,15 @@ export async function run() {
         });
 
         console.log(
-          "Request Status:" +
+            'Request Status:' +
             personResponse.status +
-            ", Person from First Team: " +
+            ', Person from First Team: ' +
             personResponse?.data?.requested_reviewers
-              ?.map((r) => r.login)
-              .join(",")
+                ?.map((r) => r.login)
+                .join(','),
         );
       } else {
-        console.log("Adding teams: " + teams);
+        console.log('Adding teams: ' + teams);
         const teamResponse = await client.rest.pulls.requestReviewers({
           owner: issue.owner,
           repo: issue.repo,
@@ -139,16 +146,16 @@ export async function run() {
           team_reviewers: teams,
         });
         console.log(
-          "Request Status:" +
+            'Request Status:' +
             teamResponse.status +
-            ", Teams: " +
-            teamResponse?.data?.requested_teams?.map((t) => t.slug).join(",")
+            ', Teams: ' +
+            teamResponse?.data?.requested_teams?.map((t) => t.slug).join(','),
         );
       }
     }
   } catch (error) {
     console.error(error);
-    core.setFailed("Unknown error" + error);
+    core.setFailed('Unknown error' + error);
     throw error;
   }
 }
