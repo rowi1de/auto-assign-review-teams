93609e5dc2ef8d57574b0a1e279b9a9ba9289ab5
Robert Wiesner <robert.wiesner@easee.com>
2026-07-14 13:38:09 +0200

    test: mock @actions/core and @actions/github via moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,11 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/.claude/'],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': ['ts-jest', {tsconfig: 'tsconfig.test.json'}]
+  },
+  moduleNameMapper: {
+    '^@actions/core$': '<rootDir>/__mocks__/@actions/core.ts',
+    '^@actions/github$': '<rootDir>/__mocks__/@actions/github.ts'
   },
   verbose: true
 }
